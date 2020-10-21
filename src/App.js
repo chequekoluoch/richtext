f659c08cfd7290d52dd2de6d54c9b4ef8e9ba5b8
makombekepha <makombekepha@gmail.com>
2020-10-21 17:59:30 +0300

    richtext

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, {useState} from 'react';
+import './App.css';
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic"
+//import CKEditor from "@ckeditor/ckeditor5-react"
+import CKEditor from '@ckeditor/ckeditor5-react';
+import parse from 'html-react-parser'
+
+function App() {
+  const [text, setText] = useState("")
+  return (
+    <div className="App">
+     <div className="editer">
+       <CKEditor
+       editor={ClassicEditor}
+       data={text}
+       onChange={(event, editor) =>{
+         const data = editor.getData()
+         setText(data)
+       }}
+       />
+     </div>
+     <div className="">
+       <h2>Content</h2>
+       <p>{parse(text)}</p>
+     </div>
+    </div>
+  );
+}
+
+export default App;
